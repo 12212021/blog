d8244b4e2ab4628f9cd94844a2e4a9fb531e558d
12212021 <12212021@bjtu.edu.cn>
2019-08-02 17:40:18 +0800

    await part

---
+++ js/async-await.js
@@ -0,0 +1,58 @@
+/* 
+async能够将异步的代码转化为同步书写的方式
+更符合人类的使用习惯 
+*/
+async function getBookByAuthorWithAwait(authorId) {
+    const books = await bookModel.fetchAll();
+    return books.filter(b => b.authorId === authorId);
+}
+
+// Promise case
+function getBookByAuthorWithAwait(authorId) {
+    return bookModel
+        .fetchAll()
+        .then(books => books.filter(b => b.authorId === authorId));
+}
+
+/* 
+async函数保证了函数一定会返回一个Promise
+但是一个Promise函数并不一定会返回一个Promise
+在这个情况下，如果authorId是falsy值，函数返回一个null
+
+async函数会将这个null自动包装为一个Promise
+caller总能能安全地调用then、catch函数
+ */
+function getBookByAuthorWithAwait(authorId) {
+    if (!authorId) {
+        return null;
+    } else {
+        return bookModel
+            .fetchAll()
+            .then(books => books.filter(b => b.authorId === authorId));
+    }
+}
+
+/* 
+避免代码太同步 
+*/
+// bad case
+async function getBookAndAuthor(authorId) {
+    const books = await bookModel.fetchAll();
+    const author = await bookModel.fetchAll(authorId);
+    return {
+        author,
+        books: books.filter(b => b.authorId === authorId),
+    };
+}
+// good case 
+async function getBookAndAuthor(authorId) {
+    const booksPromise = bookModel.fetchAll();
+    const authorPromise = bookModel.fetchAll(authorId);
+    // 保证了两个请求几乎同时发起，节省fetch数据的时间
+    const books = await booksPromise;
+    const author = await authorPromise;
+    return {
+        author,
+        books: books.filter(b => b.authorId === authorId),
+    };
+}
