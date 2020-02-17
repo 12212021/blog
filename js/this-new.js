27c21ea772649771b31163b060f750725fd37a5a
12212021 <12212021@bjtu.edu.cn>
2020-02-17 14:14:19 +0800

    vue改为在线cdn； js增加this指向

--- js/this-new.js
+++ js/this-new.js
@@ -25,7 +25,7 @@ sayName.call(obj1); // yellow
 // 4、作为构造函数进行调用的时候（js中除了一些内置函数，大部分函数都能用做构造函数）
 /* 
 函数作为构造器使用的时候，this指向了引擎为你构造的一个对象,这个对象的Constructor是该函数
-实际上，对象将不存在的属性委托给他的原型这句话的说法有点准确，准确的说法是对象对于不存在的属性，将其
+实际上，对象将不存在的属性委托给他的原型这句话的说法有点不准确，准确的说法是对象对于不存在的属性，将其
 委托给了它的构造起的原型进行查询
  */
 function CreatePerson(name = 'yuchi', age = 18) {
@@ -53,3 +53,61 @@ console.log(p) // { name: 'yellow', age: 18 }
 // 只有函数或者方法才存在prototype属性
 console.log(p.__proto__ === Object.prototype);
 
+// 5、函数内层的函数
+global.id = 'global.id';
+var a = {
+    id : 'a.id',
+    say() {
+        const innerSay = function () {
+            console.log(this.id);
+        }
+        innerSay();
+    }
+}
+a.say(); // global.id  innerSay内层函数指向了全局对象
+
+var b = {
+    id : 'b.id',
+    say() {
+        const innerSay = () => {
+            console.log(this.id);
+        }
+        innerSay();
+    }
+}
+
+b.say(); // b.id 箭头函数没有this，调用this的时候会指向上层函数的this
+
+// 6、回调函数中的this
+// (1) dom元素的回调函数
+
+/*
+<div click="onClick()"></div>  
+
+let dom = document.querySelector('div');
+dom.onclick = function () {
+
+}
+dom.addEventListener('click', function () {
+
+})
+$('div').on('click', function () {
+
+}) 
+1、全局window对象
+2、当前选中的dom元素
+3、当前选中的dom元素
+4、当前选中的dom元素（jquery）
+注：第二种和第三种绑定dom事件的方式会覆盖第一种绑定的同名事件，第一种已经不再推荐
+*/
+
+// (2) 数组常见的回调函数 map、filter、reduce
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr.filter(function (value, index, array){
+    console.log(value, index, array, this);
+}, {
+    a: 2
+});
+// fliter、map、reduce函数都有一个thisArg的参数，用来指定callback执行时this的指向
+// callback必须是普通的函数才能访问到指定的thisArg，箭头函数没有this，指向了全局对象
+// 这种类型的函数本身对this没有强需求
