43d268cdca2d9a70c20f252cbcd99a4e7b4c4b49
12212021 <12212021@bjtu.edu.cn>
2020-02-18 12:50:33 +0800

    对象的遍历和枚举

--- js/es6/Objects.js
+++ js/es6/Objects.js
@@ -142,3 +142,92 @@ Maximum call stack size exceeded的错误，是因为set、get的动态性，rel
 
 Object.setPrototypeOf(friend, dog);
 console.log(relative.getGretting());
+
+
+// 7 对象中的可枚举属性 遍历对象的手段
+var funcSymbol = Symbol();
+var attrSymbol = Symbol();
+var obj = {
+    id: 'testObj',
+    [attrSymbol]: '2',
+    say() {
+        console.log(this.id);
+    },
+    [funcSymbol]() {
+        console.log('inner');
+    }
+}
+Object.getPrototypeOf(obj).sayName = () => {
+    console.log('prototype say name');
+}
+Object.defineProperty(obj, 'name', {
+    writable: true,
+    enumerable: false
+});
+
+console.log(Object.getOwnPropertyDescriptor(obj, attrSymbol));
+// { value: '2', writable: true, enumerable: true, configurable: true } 
+// Symbol属性是可以枚举的，但是大部分操作都没有办法枚举到Symbol属性
+
+// 如何遍历对象上面的属性
+for (var attr in obj) {
+    console.log(attr); 
+    /* 
+    id
+    say
+    sayName
+
+    for...in能够遍历对象中所有的可枚举属性，但是一些原生的对象的（Array，Date）原型
+    上面的属性是不能够被遍历的
+    Symbol属性也是不能够被for...in枚举的
+    */
+}
+
+console.log(Object.keys(obj)); // [ 'id', 'say']
+// 能够遍历自身上所有的可枚举属性，但是不包括原型链上的属性（不包含继承的属性）和Symbol属性
+
+console.log(JSON.stringify(obj)); // {"id":"testObj"}
+// JSON.stringify能够JSON化自身的可枚举属性，不包括继承的，也不包含函数和Symbol属性
+
+console.log(Object.getOwnPropertyNames(obj)); // [ 'id', 'say', 'name' ]
+// 不包括Symbol属性和原型链上属性，但是包含自身不可以枚举的属性
+
+console.log(Object.getOwnPropertySymbols(obj)); // [ Symbol(), Symbol() ]
+// 返回对象自身所有的Symbol属性
+
+console.log(Reflect.ownKeys(obj)); // [ 'id', 'say', 'name', Symbol(), Symbol() ]
+// 返回自身所有的属性（可枚举、不可枚举、Symbol），但是不包含原型链上面的属性
+
+class Foo {
+    constructor () {
+        this.name = 'yu';
+        this.age = 90;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+    sayAge() {
+        console.log(this.age);
+    }
+}
+var foo = new Foo();
+console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(foo), 'sayName')); 
+/* 
+{
+  value: [Function: sayName],
+  writable: true,
+  enumerable: false,
+  configurable: true
+}
+*/
+console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(foo), 'sayAge')); 
+/* 
+{
+  value: [Function: sayAge],
+  writable: true,
+  enumerable: false,
+  configurable: true
+}
+*/
+console.log(Object.keys(foo)); // ['name', 'age']
+// class定义的方法都对方在对象的原型链上，但是是不可以枚举的
