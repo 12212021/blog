c3eda6c83acf884ab6bdaf48b7a3525a1c92b9c8
12212021 <12212021@bjtu.edu.cn>
2020-02-20 17:11:13 +0800

    为什么说await、async是语法糖

--- js/async-await.js
+++ js/async-await.js
@@ -66,3 +66,69 @@ Error handing
 3、return Promise.reject(error)等同于throw一个error，不推荐
 4、因为await后的表达式返回一个Promise，所以也可以使用catch语句来捕获所有的错误。
  */
+
+
+
+// 这是一个模拟的http请求
+const getId = () => {
+    return new Promise((resolve, reject) => {
+        let id = {
+            name: 'yuchi',
+            age: 12
+        };
+        let errorMsg = 'http requests error, code 404';
+        setTimeout(() => {
+            if (Math.random() > 0.5) {
+                resolve(id);
+            } else {
+                reject(errorMsg);
+            }
+        }, 1000)
+    })
+}
+
+
+// 生成器函数 演示的是生成器是如何作为await、async函数的基础的
+function* requestUserId() {
+    try {
+        // yeild关键字可以任务是await关键字的简化版
+        // 在这个生成器函数中yeild将线程的控制权交还给mainThread函数（可以简单理解为主线程）
+        /*
+        await关键字还可以直接抛出非promise的数值，如await 100，但是await会自动将这个值包裹为一个Promise
+        对于Promise而言直接直接返回这个promise，或者新创建个一个Promise，直接resolve接收到的Promise对象
+         */
+
+        // await 100情况如下
+        /* 
+        const res = yield (new Promise((resolve, reject) => {
+            resolve(100);
+        } ));
+        */
+        // await http请求，情况如下
+        const res = yield (new Promise((resolve, reject) => {
+            resolve(getId());
+        }))
+        console.log('this id user id message', res);
+    } catch (err) {
+        console.log(err, 'from main Thread');
+    }
+
+}
+
+const mainThread = () => {
+    let it = requestUserId();
+    
+    // 下面部分代码的执行都是async、await关键字来实现了，对于用户而言是不可见的，体现了语法糖的本质
+    // 执行到yeild关键字的右半边部分 拿到相关的Promise对象
+    let ajaxPromise = it.next().value;
+    // 通过next、throw方法将线程的控制权返还给子协程（requestUserId）
+    ajaxPromise.then(res => {
+        it.next(res)
+    }).catch(err => {
+        it.throw(err);
+    })
+}
+
+// async、await关键字是语法糖，它是由Promise、Iterator、generator三个语言层面的特性来实现异步代码采用同步的方式来书写
+
+mainThread();
