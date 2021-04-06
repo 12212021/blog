e3dfad3840427278798e4dfd89ae6dbfd94c96fa
yujianghua <yujianghua@baidu.com>
2021-04-06 13:32:56 +0800

    promise.all

--- js/lib-functions.js
+++ js/lib-functions.js
@@ -405,7 +405,7 @@ function promisefy(fn) {
 }
 
 /**
- * 
+ *
  * @param {Function} fn promise类型函数
  * @returns 返回一个callback类型的函数
  * @description 默认callback回调函数，参数类型为(err, data)，用户使用的时候需要将cb作为最后一个参数传递
@@ -419,4 +419,29 @@ function callbackfy(fn) {
             cb(err, undefined);
         });
     }
+}
+
+/**
+ *
+ * @param {Array} args 一个包含promsie的可迭代对象
+ * @returns 如果所有的promise都返回，则返回一个数组，数组内包含了所有promise，否则返回第一个reject的内容
+ */
+function PromiseAll(args) {
+    return new Promise((resolve, reject) => {
+        let promiseNumber = args.length;
+        let count = 0;
+        let resolved = new Array(promiseNumber);
+        for (let index = 0; index < promiseNumber; index++) {
+            let p = args[index];
+            p.then(response => {
+                count += 1;
+                rejected[index] = response;
+                if (count === promiseNumber) {
+                    resolve(resolved);
+                }
+            }).then(err => {
+                reject(err);
+            })
+        }
+    })
 }
